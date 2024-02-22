736d6dc6f50374b0551edd80b67a05d0ab1b2c86
İsmail Özsaygı <devozsaygi@gmail.com>
2024-02-22 12:23:34 +0300

    Added basic platform service

---
+++ Assets/AAA/Source/Platform/Runtime/PlatformService.cs
@@ -0,0 +1,15 @@
+using AAA.Source.Service.Runtime;
+
+namespace AAA.Source.Platform.Runtime
+{
+    public class PlatformService : ApplicationService
+    {
+        public override void Initialize()
+        {
+        }
+
+        public override void Shutdown()
+        {
+        }
+    }
+}
