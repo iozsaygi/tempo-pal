bc1bca133a97ec5966a122c129ba89fd6656db82
İsmail Özsaygı <devozsaygi@gmail.com>
2024-02-23 12:42:38 +0300

    Added metronome service

---
+++ Assets/AAA/Source/Metronome/Runtime/MetronomeService.cs
@@ -0,0 +1,15 @@
+using AAA.Source.Service.Runtime;
+
+namespace AAA.Source.Metronome.Runtime
+{
+    public class MetronomeService : ApplicationService
+    {
+        public override void Initialize(ServiceController serviceController)
+        {
+        }
+
+        public override void Shutdown()
+        {
+        }
+    }
+}
