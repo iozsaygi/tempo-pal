643ef22073ffed2fc5ec62cd563059acf86fc5e6
İsmail Özsaygı <devozsaygi@gmail.com>
2024-02-23 12:49:52 +0300

    Added debugger service

---
+++ Assets/AAA/Source/Debugger/Runtime/DebuggerService.cs
@@ -0,0 +1,15 @@
+using AAA.Source.Service.Runtime;
+
+namespace AAA.Source.Debugger.Runtime
+{
+    public class DebuggerService : ApplicationService
+    {
+        public override void Initialize(ServiceController serviceController)
+        {
+        }
+
+        public override void Shutdown()
+        {
+        }
+    }
+}
