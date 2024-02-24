01f03fc7698e487ab07243443ca39104ab602927
İsmail Özsaygı <devozsaygi@gmail.com>
2024-02-24 15:45:52 +0300

    Added metronome bridge creation

--- Assets/AAA/Source/Metronome/Runtime/MetronomeController.cs
+++ Assets/AAA/Source/Metronome/Runtime/MetronomeController.cs
@@ -1,4 +1,6 @@
 using AAA.Source.Debugger.Runtime;
+using UnityEngine;
+using Object = UnityEngine.Object;
 
 // ReSharper disable InconsistentNaming
 
@@ -6,14 +8,37 @@ namespace AAA.Source.Metronome.Runtime
 {
     public class MetronomeController
     {
+        // Dependencies.
         private readonly DebuggerService debuggerService;
-
+        private readonly MetronomeService metronomeService;
         private readonly MetronomeSettings metronomeSettings;
 
-        public MetronomeController(DebuggerService debuggerService, MetronomeSettings metronomeSettings)
+        // Required references for audio bridge.
+        private GameObject gameObject;
+        private AudioSource audioSource;
+
+        public MetronomeController(DebuggerService debuggerService, MetronomeService metronomeService,
+            MetronomeSettings metronomeSettings)
         {
             this.debuggerService = debuggerService;
+            this.metronomeService = metronomeService;
             this.metronomeSettings = metronomeSettings;
+
+            InstantiateAudioBridge();
+        }
+
+        private void InstantiateAudioBridge()
+        {
+            // Create required objects.
+            gameObject = new GameObject(nameof(MetronomeController));
+            audioSource = gameObject.AddComponent<AudioSource>();
+
+            // Update audio source settings.
+            audioSource.playOnAwake = false;
+            audioSource.loop = false;
+            audioSource.clip = metronomeSettings.AudioClip;
+
+            gameObject.transform.SetParent(metronomeService.transform, true);
         }
     }
 }
