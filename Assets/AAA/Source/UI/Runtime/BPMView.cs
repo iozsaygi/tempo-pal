8743b46f1c7653d06b2d0580ad422928964c3e5b
İsmail Özsaygı <devozsaygi@gmail.com>
2024-03-02 19:03:21 +0300

    Added basic logic for BPMView

---
+++ Assets/AAA/Source/UI/Runtime/BPMView.cs
@@ -0,0 +1,13 @@
+using TMPro;
+using UnityEngine;
+
+// ReSharper disable InconsistentNaming
+
+namespace AAA.Source.UI.Runtime
+{
+    [DisallowMultipleComponent]
+    [RequireComponent(typeof(TextMeshProUGUI))]
+    public class BPMView : MonoBehaviour
+    {
+    }
+}
